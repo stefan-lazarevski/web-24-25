f7c83fb8e7a6bc7dc01c8d2194a483fb705861e1
stefan-lazarevski <166828400+stefan-lazarevski@users.noreply.github.com>
2024-11-27 19:50:45 +0100

    Delete Lab directory

--- Lab/src/main/java/mk/ukim/finki/wp/lab/model/Exceptions/InvalidArgumentsException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.model.Exceptions;
-
-public class InvalidArgumentsException extends RuntimeException{
-    public InvalidArgumentsException() {
-        super("Invalid arguments");
-    }
-}
