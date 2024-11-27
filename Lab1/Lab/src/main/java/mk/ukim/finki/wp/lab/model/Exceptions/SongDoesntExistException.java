c2c40de21707299372e69735f073e215b83185c2
stefan-lazarevski <166828400+stefan-lazarevski@users.noreply.github.com>
2024-11-27 15:44:14 +0100

    Delete Lab1/Lab directory

--- Lab1/Lab/src/main/java/mk/ukim/finki/wp/lab/model/Exceptions/SongDoesntExistException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.model.Exceptions;
-
-public class SongDoesntExistException extends RuntimeException{
-    public SongDoesntExistException() {
-        super("Artist dose not exist");
-    }
-}
